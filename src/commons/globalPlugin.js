cf28228a701922b873e29051b48e0716a520896d
TS <sannxt.work@gmail.com>
2023-02-23 17:16:53 +0700

    update

--- src/commons/globalPlugin.js
+++ src/commons/globalPlugin.js
@@ -3,5 +3,16 @@ import toasts from '@/lib/toast';
 
 export function pluginInstall(app) {
     app.config.globalProperties.$constants = constants;
-    app.config.globalProperties.$toast = toasts
+    app.config.globalProperties.$toast = toasts;
+
+    app.mixin({
+        mounted() {
+            const me = this;
+            if(me.$el && me.$el.getVueInstance) {
+                me.$el.getVueInstance = () => {
+                    return this;
+                }
+            }
+        }
+    })
 }
